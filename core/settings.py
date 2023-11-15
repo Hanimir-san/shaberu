d87cbf630ed4ee6253beaab57cef25e0f758f19a
hanimir-san <fou_lou2003@hotmail.com>
2023-11-15 18:08:59 +0100

    Updated requirements for AI functionality. Updated settings.

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os, environ
+import os, random, string
+from pathlib import Path
+from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
